efc242d78743a796ad3955e1f4decc0ec976251a
ian-r-rose <ian.r.rose@gmail.com>
2016-10-11 15:00:52 -0700

    Move new file creation into the RealtimeDocumentModel.

--- src/realtime/model.ts
+++ src/realtime/model.ts
@@ -13,30 +13,30 @@ import {
   DocumentModel, TextModelFactory
 } from '../docregistry/default';
 
-import {
-  realtimeDoc, realtimeModel, collaborativeString
-} from './auth';
-
 declare let gapi : any;
+
+const RT_MIMETYPE = 'application/vnd.google-apps.drive-sdk';
 
 export 
 class RealtimeDocumentModel extends DocumentModel {
   constructor(languagePreference?: string) {
     super(languagePreference);
+    this.createOrLoadRealtimeFile();
   }
 
   registerCollaborative(): void {
-    this._collaborativeString = collaborativeString;
     this.fromString(this._collaborativeString.getText());
-    this._collaborativeString.addEventListener(gapi.drive.realtime.EventType.TEXT_INSERTED,
-                                               (evt : any) => {
-                                                 this.fromString(this._collaborativeString.getText());
-                                               });
-    this._collaborativeString.addEventListener(gapi.drive.realtime.EventType.TEXT_DELETED,
-                                               (evt : any) => {
-                                                 this.fromString(this._collaborativeString.getText());
-                                               });
+    this._collaborativeString.addEventListener(
+      gapi.drive.realtime.EventType.TEXT_INSERTED,
+      (evt : any) => {
+        this.fromString(this._collaborativeString.getText());
+      });
 
+    this._collaborativeString.addEventListener(
+          gapi.drive.realtime.EventType.TEXT_DELETED,
+          (evt : any) => {
+            this.fromString(this._collaborativeString.getText());
+          });
   }
 
   fromString(value: string): void {
@@ -44,19 +44,64 @@ class RealtimeDocumentModel extends DocumentModel {
     this._collaborativeString.setText(value);
   }
 
+  loadRealtimeFile( fileId : string) : void {
+    let _this = this;
+    console.log("Attempting to load realtime file " + fileId);
+    gapi.drive.realtime.load( fileId, (doc : any ):any => {
+      _this._realtimeDoc = doc;
+      _this._realtimeModel = _this._realtimeDoc.getModel();
+      _this._collaborativeString = 
+        _this._realtimeModel.getRoot().get("collabstring");
+      _this.registerCollaborative();
+    });
+  }
 
+  createRealtimeFile() : void {
+    let _this = this;
+    gapi.client.drive.files.create({
+      'resource': {
+        mimeType: RT_MIMETYPE,
+        name: 'jupyterlab_realtime_file'
+        }
+    }).then( (response : any) : any => {
+         _this._fileId = JSON.parse(response.body).id;
+         gapi.drive.realtime.load( _this._fileId, (doc : any ):any => {
+           _this._realtimeDoc = doc;
+           _this._realtimeModel = _this._realtimeDoc.getModel();
+           _this._collaborativeString = 
+             _this._realtimeModel.createString("I am a collaborative string");
+           _this._realtimeModel.getRoot()
+             .set("collabstring", _this._collaborativeString);
+           console.log("setup realtime document "+_this._fileId);
+           _this.registerCollaborative();
+         });
+       });
+  }
+
+  createOrLoadRealtimeFile() : void {
+    let _this = this;
+    gapi.client.load('drive', 'v3').then( function() {
+      let query : string = (window as any).location.search;
+      if (query) {
+        _this._fileId = query.slice(1);
+        _this.loadRealtimeFile(_this._fileId);
+      }
+      else {
+        _this.createRealtimeFile();
+      }
+    });
+  }
+
+  private _fileId : string = '';
+  private _realtimeDoc : gapi.drive.realtime.Document = null;
+  private _realtimeModel : gapi.drive.realtime.Model = null;
   private _collaborativeString : gapi.drive.realtime.CollaborativeString = null;
 }
 
 
 export
 class RealtimeTextModelFactory extends TextModelFactory {
-  constructor() {
-    super();
-    this._doc = realtimeDoc;
-    this._model = realtimeModel;
-    this._collaborativeString = collaborativeString;
-  }
+
   /**
    * The name of the model type.
    *
@@ -69,11 +114,7 @@ class RealtimeTextModelFactory extends TextModelFactory {
 
   createNew(languagePreference?: string) : RealtimeDocumentModel {
     let doc = new RealtimeDocumentModel(languagePreference);
-    doc.registerCollaborative();
     return doc;
   }
 
-  private _doc : gapi.drive.realtime.Document = null;
-  private _model : gapi.drive.realtime.Model = null;
-  private _collaborativeString : gapi.drive.realtime.CollaborativeString = null;
 }
